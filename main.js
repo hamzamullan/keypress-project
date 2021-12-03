9a19483336015781b88984dfecc3f419bae7d76a
hamzamullan <80555783+hamzamullan@users.noreply.github.com>
2021-12-02 18:09:56 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// Create a reference for the canvas
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
 img_width = 300;
 img_height = 100;
@@ -19,7 +20,7 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
-//Write a code to grab the key-pressed event
+window.addEventListener("keydown",my_keydown);
 
 function my_keydown(e)
 {
@@ -27,28 +28,43 @@ function my_keydown(e)
 	console.log(keyPressed);
 	
 		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-		//write a code to check the type of key pressed
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML = "you pressed alphabet key";
+		}
+		else if(keyPressed >= 48 && keyPressed <= 57){
+            numberkey();
+			document.getElementById("d1").innerHTML = "you pressed number key";
+		}
+		else if(keyPressed >= 37 && keyPressed <= 40){
+			arrowkey();
+			document.getElementById("d1").innerHTML = "you pressed arrow key";
+		}
+		else if((keyPressed == 17)||(keyPressed == 18)||(keyPressed == 27)){
+        specialkey();
+		document.getElementById("d1").innerHTML = "you pressed ctrl/esc/alt key";
+		}
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
 }
 
-function aplhabetkey()
-{
-	//upload respective image with the message. 
-
+function aplhabetkey(){
+	img_image="Alpkey.png";
+	add()
 }
-function numberkey()
-{
-	
+function numberkey(){
+	img_image="numkey.png";
+	add();
 }
-function arrowkey()
-{
+function arrowkey(){
+	img_image="Arrkey.png";
+	add();
 }
-function specialkey()
-{
-	
+function specialkey(){
+	img_image="spkey.png";
+	add();
 }
 function otherkey()
 {
